428c26be3ea85def0f1d2da6bac82a2aaa516e91
lorena <lguachi@yachaytech.edu.ec>
2018-02-25 00:47:31 -0500

    ultimo commit

--- ClassLibrary3/Class1.cs
+++ ClassLibrary3/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +7,5 @@ namespace ClassLibrary3
 {
     public class Class1
     {
-
     }
 }
