39197bf6a4f72c405b5b7fa1b67d9154f93bef24
loreangeles <36804703+loreangeles@users.noreply.github.com>
2018-02-25 00:39:41 -0500

    Update Class1.cs
    cambios desde la web

--- ClassLibrary3/Class1.cs
+++ ClassLibrary3/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +7,6 @@ namespace ClassLibrary3
 {
     public class Class1
     {
+        cambios desde la web
     }
 }
